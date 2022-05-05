102cbed0a11e9ec4c219d4f3f815c3c2ae170d40
_XiaoTian <istianlei@qq.com>
2022-05-06 02:12:30 +0800

    updateTextNode

--- tiny-react/src/TinyReact/createDOMElement.js
+++ tiny-react/src/TinyReact/createDOMElement.js
@@ -12,6 +12,8 @@ export default function createDOMElement (virtualDOM) {
     updateNodeElement(virtualDOM, newElement)
   }
 
+  newElement._virtualDOM = virtualDOM
+
   // 递归创建子节点
   virtualDOM.children.forEach(child => {
     mountElement(child, newElement)
--- tiny-react/src/index.js
+++ tiny-react/src/index.js
@@ -21,7 +21,27 @@ const virtualDOM = (
     </div>
   )
 
-  const container = document.getElementById('root')
+  const modifyVirtualDOM = (
+    <div className="container">
+      <h1 data-test="test">你好 Tiny React</h1>
+      <h2>(编码必杀技)</h2>
+      <div>
+        嵌套1 <div>嵌套 1.1</div>
+      </div>
+      <h3>(观察: 这个将会被改变)</h3>
+      {2 == 1 && <div>如果2和1相等渲染当前内容</div>}
+      {2 == 2 && <div>2</div>}
+      <span>这是一段内容 我被修改了</span>
+      <button onClick={() => alert("你好")}>点击我</button>
+      <h3>这个将会被删除</h3>
+      2, 3
+      <input type="text" value="123"></input>
+      <input type="checkbox"  checked={true} />
+      <input type="checkbox"  checked={false} />
+    </div>
+  )
+
+  const root = document.getElementById('root')
 
   const Demo = () => <span>&hearts;</span>
   const Heart = (props) => <div>
@@ -31,8 +51,11 @@ const virtualDOM = (
     {props.title}
   </div>
 
-  // TinyReact.render(virtualDOM, container)
-  // TinyReact.render(<Heart title="Hello React" />, container)
+  TinyReact.render(virtualDOM, root)
+  setTimeout(() => {
+    TinyReact.render(modifyVirtualDOM, root)
+  }, 2000)
+  // TinyReact.render(<Heart title="Hello React" />, root)
 
   class Alert extends TinyReact.Component {
     constructor(props) {
@@ -47,6 +70,6 @@ const virtualDOM = (
       </div>
     }
   }
-  TinyReact.render(<Alert name="zhangsan" age={18} />, container)
+  // TinyReact.render(<Alert name="zhangsan" age={18} />, root)
 
   console.log(virtualDOM)
