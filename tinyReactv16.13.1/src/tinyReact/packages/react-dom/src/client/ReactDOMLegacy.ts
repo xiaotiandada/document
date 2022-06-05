ae5b9f2670541bed847bdb084ecab25eec8866bd
xiaotian <istianlei@qq.com>
2022-06-06 00:32:24 +0800

    legacyCreateRootFromDOMContainer

--- tinyReactv16.13.1/src/tinyReact/packages/react-dom/src/client/ReactDOMLegacy.ts
+++ tinyReactv16.13.1/src/tinyReact/packages/react-dom/src/client/ReactDOMLegacy.ts
@@ -1,7 +1,117 @@
 import type { Container } from './ReactDOMHostConfig';
-import { isValidContainer } from './ReactDOMRoot';
+import { createLegacyRoot, isValidContainer } from './ReactDOMRoot';
 import invariant from '../../../shared/invariant';
 import type { ReactNodeList } from '../../../shared/ReactTypes';
+import type { RootType } from './ReactDOMRoot';
+import {
+  DOCUMENT_NODE,
+  ELEMENT_NODE,
+  COMMENT_NODE,
+} from '../shared/HTMLNodeType';
+import { ROOT_ATTRIBUTE_NAME } from '../shared/DOMProperty';
+
+const __DEV__ = true;
+
+let topLevelUpdateWarnings;
+let warnedAboutHydrateAPI = false;
+
+function getReactRootElementInContainer(container: any) {
+  if (!container) {
+    return null;
+  }
+
+  if (container.nodeType === DOCUMENT_NODE) {
+    return container.documentElement;
+  } else {
+    return container.firstChild;
+  }
+}
+
+function shouldHydrateDueToLegacyHeuristic(container: any) {
+  const rootElement = getReactRootElementInContainer(container);
+  return !!(
+    rootElement &&
+    rootElement.nodeType === ELEMENT_NODE &&
+    rootElement.hasAttribute(ROOT_ATTRIBUTE_NAME)
+  );
+}
+
+/**
+ * 判断是否为服务器端渲染 如果不是服务器端渲染
+ * 清空 container 容器中的节点
+ */
+function legacyCreateRootFromDOMContainer(
+  container: Container,
+  forceHydrate: boolean
+): RootType {
+  // container => <div id="root"></div>
+  // 检测是否为服务器端渲染
+  const shouldHydrate =
+    forceHydrate || shouldHydrateDueToLegacyHeuristic(container);
+
+  console.log('shouldHydrate', shouldHydrate);
+  console.log('container', container);
+
+  // First clear any existing content.
+  // 如果不是服务器端渲染
+  if (!shouldHydrate) {
+    let warned = false;
+    let rootSibling;
+    // 开启循环 删除 container 容器中的节点
+    while ((rootSibling = container.lastChild)) {
+      if (__DEV__) {
+        if (
+          !warned &&
+          rootSibling.nodeType === ELEMENT_NODE &&
+          (rootSibling as any).hasAttribute(ROOT_ATTRIBUTE_NAME)
+        ) {
+          warned = true;
+          console.error(
+            'render(): Target node has markup rendered by React, but there ' +
+              'are unrelated nodes as well. This is most commonly caused by ' +
+              'white-space inserted around server-rendered markup.'
+          );
+        }
+      }
+      // 删除 container 容器中的节点
+      container.removeChild(rootSibling);
+      /**
+       * 为什么要清除 container 中的元素 ?
+       * 为提供首屏加载的用户体验, 有时需要在 container 中放置一些占位图或者 loading 图
+       * 就无可避免的要向 container 中加入 html 标记.
+       * 在将 ReactElement 渲染到 container 之前, 必然要先清空 container
+       * 因为占位图和 ReactElement 不能同时显示
+       *
+       * 在加入占位代码时, 最好只有一个父级元素, 可以减少内部代码的循环次数以提高性能
+       * <div>
+       *  <p>placement<p>
+       *  <p>placement<p>
+       *  <p>placement<p>
+       * </div>
+       */
+    }
+  }
+
+  if (__DEV__) {
+    if (shouldHydrate && !forceHydrate && !warnedAboutHydrateAPI) {
+      warnedAboutHydrateAPI = true;
+      console.warn(
+        'render(): Calling ReactDOM.render() to hydrate server-rendered markup ' +
+          'will stop working in React v17. Replace the ReactDOM.render() call ' +
+          'with ReactDOM.hydrate() if you want React to attach to the server HTML.'
+      );
+    }
+  }
+
+  return createLegacyRoot(
+    container,
+    shouldHydrate
+      ? {
+          hydrate: true,
+        }
+      : undefined
+  );
+}
 
 function legacyRenderSubtreeIntoContainer(
   parentComponent: any,
@@ -10,6 +120,20 @@ function legacyRenderSubtreeIntoContainer(
   forceHydrate: boolean,
   callback?: Function
 ) {
+  let root: RootType = container._reactRootContainer as any;
+  let fiberRoot;
+
+  if (!root) {
+    // Initial mount
+    // legacyCreateRootFromDOMContainer: 清除container的所有子DOM
+    root = container._reactRootContainer = legacyCreateRootFromDOMContainer(
+      container,
+      forceHydrate
+    );
+
+    console.log('root', root);
+  }
+
   return {};
 }
 
--- tinyReactv16.13.1/src/tinyReact/packages/react-dom/src/client/ReactDOMRoot.ts
+++ tinyReactv16.13.1/src/tinyReact/packages/react-dom/src/client/ReactDOMRoot.ts
@@ -4,26 +4,54 @@ import {
   DOCUMENT_NODE,
   DOCUMENT_FRAGMENT_NODE,
 } from '../shared/HTMLNodeType';
+import type { Container } from './ReactDOMHostConfig';
+import {
+  BlockingRoot,
+  ConcurrentRoot,
+  LegacyRoot,
+} from '../../../shared/ReactRootTags';
+import type { RootTag } from '../../../shared/ReactRootTags';
 
 export type RootType = {
   render(children: any): void;
   unmount(): void;
   _internalRoot: any;
 };
+export type RootOptions = {
+  hydrate?: boolean;
+  hydrationOptions?: {
+    onHydrated?: (suspenseNode: Comment) => void;
+    onDeleted?: (suspenseNode: Comment) => void;
+  };
+};
+
+function ReactDOMBlockingRoot(
+  container: Container,
+  tag: RootTag,
+  options: void | RootOptions
+) {
+  this._internalRoot = {};
+}
+
+export function createLegacyRoot(
+  container: Container,
+  options?: RootOptions
+): RootType {
+  return {} as RootType;
+}
 
 /**
  *
  * @param node mixed
- * @returns
- */
-
-/**
+ *
  * 判断 node 是否是符合要求的 DOM 节点
  * 1. node 可以是元素节点
  * 2. node 可以是 document 节点
  * 3. node 可以是 文档碎片节点
  * 4. node 可以是注释节点但注释内容必须是 react-mount-point-unstable
  * 		react 内部会找到注释节点的父级 通过调用父级元素的 insertBefore 方法, 将 element 插入到注释节点的前面
+ *
+ * https://developer.mozilla.org/zh-CN/docs/Web/API/Node/nodeType
  */
 export function isValidContainer(node: any): boolean {
   return !!(
